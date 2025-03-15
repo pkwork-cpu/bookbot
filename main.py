9a1a5147608c5a2f9ad919a21bc62eb45f8b6f3e
pkwork-cpu <kermani.pedram6@gmail.com>
2025-03-15 17:10:16 -0500

    CH2L4 Count Words

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_content = f.read()
     return file_content
+def count_words(book_text_string):
+    list_of_words = book_text_string.split()
+    return  len(list_of_words)
 def main():
-    print (get_book_text("books/frankenstein.txt"))
+    booktext = get_book_text("books/frankenstein.txt")
+    wordcount = count_words(booktext)
+    print (f"{wordcount} words found in the document")
 main()
