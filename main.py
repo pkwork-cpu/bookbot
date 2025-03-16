3e42f922f9e5155534023127e2f4da80a5a9bac5
pkwork-cpu <kermani.pedram6@gmail.com>
2025-03-16 11:04:59 -0500

    CH3L1 Print Report

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ def get_book_text(filepath):
     return book_string
 def main():
     book_string = get_book_text("books/frankenstein.txt")
-    from stats import get_num_words
-    from stats import get_charectors_dict
+    from stats import get_num_words, get_charectors_dict, char_dict_list, sort_on
     wordcount = get_num_words(book_string)
     charector_dict = get_charectors_dict(book_string)
-    print (f"{wordcount} words found in the document\n\nCharector count:\n{charector_dict}")
-
+    sort_list = char_dict_list(charector_dict)
+    sort_list.sort(reverse=True, key=sort_on)
+    print ("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+    print (f"Found {wordcount} total words\n--------- Character Count -------")
+    for i in range(len(sort_list)) :
+        charector = sort_list[i]["char"]
+        if charector.isalpha() == True :
+            print (f"{sort_list[i]["char"]}: {sort_list[i]["numb"]}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -4,8 +4,6 @@ def get_num_words(books_text_string):
     # return the number of items in list
     return  len(list_of_words)
 
-
-
 def get_charectors_dict(books_text_string):
     all_low = books_text_string.lower()
     # Initialize an empty dictionary to store character counts
@@ -17,3 +15,22 @@ def get_charectors_dict(books_text_string):
         else:
             char_count[char] = 1
     return char_count
+
+def char_dict_list(char_dict):
+    #char_dict.sort(reverse=False)
+    list= []
+    for char in char_dict:
+        # char_dict example: {'t': 29493, 'h': 19176,
+        # char = charector and char_dict[char] = number
+        list.append({"char": char, "numb": char_dict[char]})
+    
+    """
+    to return a sorted list with out extra function
+    list.sort(key=lambda x: x["numb"], reverse=True)
+    """
+    return list
+
+# A function that takes a dictionary and returns the value of the "numb" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(dict):
+    return dict["numb"]
