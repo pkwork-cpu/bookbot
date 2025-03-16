0eb45e8574da604f72f34ea5dd0a4051f662d635
pkwork-cpu <kermani.pedram6@gmail.com>
2025-03-16 12:56:49 -0500

    CH3L2 Arguments

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        book_string = f.read()
-    return book_string
 def main():
-    book_string = get_book_text("books/frankenstein.txt")
-    from stats import get_num_words, get_charectors_dict, char_dict_list, sort_on
+    # import command-line argument script name and path to book
+    import sys
+    # if path to file is not specified will prompt instruction on how to use program
+    if len(sys.argv) < 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    # import data analysis functions from stats.py
+    from stats import get_book_text, get_num_words, get_charectors_dict, char_dict_list, sort_on
+    # use path to file(sys.argv[1] to read and turn file into string)
+    book_string = get_book_text(sys.argv[1])
+    # count the number of words 
     wordcount = get_num_words(book_string)
+    # Count characters save result into dictonary exmp: {'p': 6121, 'r': 20818, ect}
     charector_dict = get_charectors_dict(book_string)
+    '''
+    Creat a list of dictionaries example:
+    sort_list = [
+    {'char': 'e', 'numb': 44538},
+    {'char': 't', 'numb': 29493}
+    ]
+    '''
     sort_list = char_dict_list(charector_dict)
+    # sort list by highest used charector to least
     sort_list.sort(reverse=True, key=sort_on)
+    #finally prints word count and list of charectors and number of charectors
     print ("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
     print (f"Found {wordcount} total words\n--------- Character Count -------")
     for i in range(len(sort_list)) :
--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_book_text(filepath):
+    # use path to file(sys.argv[1] to read and turn file into string)
+    with open(filepath) as f:
+        book_string = f.read()
+    return book_string
 def get_num_words(books_text_string):
     # Split String word by word and store in list
     list_of_words = books_text_string.split()
@@ -15,9 +20,14 @@ def get_charectors_dict(books_text_string):
         else:
             char_count[char] = 1
     return char_count
-
+'''
+    Creat a list of dictionaries example:
+    sort_list = [
+    {'char': 'e', 'numb': 44538},
+    {'char': 't', 'numb': 29493}
+    ]
+'''
 def char_dict_list(char_dict):
-    #char_dict.sort(reverse=False)
     list= []
     for char in char_dict:
         # char_dict example: {'t': 29493, 'h': 19176,
@@ -25,7 +35,7 @@ def char_dict_list(char_dict):
         list.append({"char": char, "numb": char_dict[char]})
     
     """
-    to return a sorted list with out extra function
+    to return a sorted list with out extra function:
     list.sort(key=lambda x: x["numb"], reverse=True)
     """
     return list
